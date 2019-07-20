6ed5f423de200ad4772aba1ce4d73723038bb184
Darek <my_email@example.com>
2019-07-20 13:10:41 +0200

    feat: edytorek is done

--- src/js/main.js
+++ src/js/main.js
@@ -17,4 +17,48 @@
 // place your code below
 
 
-console.log(`Hello world!`);
+// console.log(`Hello world!`);
+// localStorage.setItem('edytor','treść');
+// const person = {
+//     name: 'maciek',
+//     age: 31
+// };
+// //console.log(JSON.stringify(person));
+// const jsonPerson = JSON.stringify(person);
+// localStorage.setItem('osoba',jsonPerson);
+
+// const personStringify = localStorage.getItem('osoba');
+// const personRead = JSON.parse(personStringify);
+
+// console.log(personRead.name);
+
+const inputField = document.querySelector('.inputField--js');
+if (localStorage.getItem('inputField')) {
+    inputField.value=localStorage.getItem('inputField');
+};
+inputField.addEventListener('keyup', (e) => {
+    //console.log(e.target.value);
+    localStorage.setItem('inputField',e.target.value);
+});
+
+const inputArea = document.querySelector('.edit--js');
+const saveKey = document.querySelector('.saveBtn--js');
+const loadKey = document.querySelector('.loadBtn--js');
+
+const writeToTextArea = function () {
+    inputArea.value = localStorage.getItem('edytor');
+};
+
+if (localStorage.getItem('edytor')) {
+    console.log('cos');
+    writeToTextArea();
+}
+
+saveKey.addEventListener('click', (e) => {
+    localStorage.setItem('edytor', inputArea.value);
+});
+
+loadKey.addEventListener('click', writeToTextArea);
+
+
+
